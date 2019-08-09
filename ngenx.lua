1b5de47a39c18d4481dfa622c4749969ede0ed1a
zenith391 <pawalamde@gmail.com>
2019-08-09 16:14:35 +0200

    Ngenx update

--- ngenx.lua
+++ ngenx.lua
@@ -8,7 +8,7 @@ local args, options = require("shell").parse(...)
 
 local function getNgenx()
 	for _, v in pairs(shin32.getProcesses()) do
-		if v.name == "ngenx" then
+		if v.name == "ngenx-daemon" then
 			return true, v
 		end
 	end
@@ -17,7 +17,39 @@ end
 
 -- Server
 local function server()
-	
+	while true do
+		local socket = network.listen(80, "modem")
+		local request = socket:read()
+		local lines = ifOr(string.endsWith("\n"), string.split(request, "\n"), {request})
+
+		local header = lines[1]
+		local headerSplit = string.split(header)
+		local properties = {}
+		if headerSplit[1] ~= "OHTP/1.0" or headerSplit[2] ~= "GET" then
+			socket:write("403")
+			socket:close() -- invalid request
+		end
+		if #lines > 1 then
+			for i=2,#lines do
+				local line = lines[i]
+				local lineSplit = string.split(line, ":")
+				properties[lineSplit[1]] = lineSplit[2]
+			end
+		end
+
+		local stream = io.open(headerSplit[3], "r")
+		if not stream then
+			socket:write("404")
+			socket:close()
+		else
+			local content = stream:read("*a")
+			local response = "200"
+			response = response .. "\nContent-Type: text/ohml"
+			response = response .. "\n\n" .. content
+			socket:write(response)
+			socket:close()
+		end
+	end
 end
 
 if args[1] == "start" then
@@ -33,6 +65,7 @@ if args[1] == "start" then
 		p:detach()
 		print("Started")
 	end
+	return
 end
 
 if args[1] == "stop" then
@@ -43,4 +76,7 @@ if args[1] == "stop" then
 	else
 		print("Not running")
 	end
+	return
 end
+
+io.stderr:write("Usage: ngenx <start/stop>\n")
