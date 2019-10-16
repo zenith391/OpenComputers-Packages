006a7c930045d937d3b959ff9f8b6f0f25c59cdf
zenith391 <pawalamde@gmail.com>
2019-10-16 14:55:43 +0200

    Minescape Script support

--- minescape.lua
+++ minescape.lua
@@ -27,6 +27,88 @@ local cy = 3
 local cx = 1
 local objects = {}
 
+local scriptProcesses = {}
+local scriptId = 1
+local scriptEnv
+
+local function objectWrapper(obj)
+	if obj.type == "canvas" then
+		local wrapper = {
+			drawText = function(x, y, text)
+				if obj.drawHandler ~= nil then
+					obj.drawHandler("text", x, y, text)
+				end
+			end,
+			fillRect = function(x, y, width, height, char)
+				if obj.drawHandler ~= nil then
+					obj.drawHandler("fill", x, y, width, height, char)
+				end
+			end,
+			setBackground = function(color)
+				if obj.drawHandler ~= nil then
+					obj.drawHandler("setbg", color)
+				end
+			end,
+			setForeground = function(color)
+				if obj.drawHandler ~= nil then
+					obj.drawHandler("setfg", color)
+				end
+			end
+		}
+		return wrapper
+	end
+	return nil
+end
+
+local function makeScriptEnv()
+	scriptEnv = {
+		_G = scriptEnv,
+		sleep = os.sleep,
+		navigator = {
+			appName = "Minescape",
+			appCodeName = "Mozarella",
+			product = "Geeko",
+			appVersion = 0.9,
+			userAgent = "Minescape/0.9 Geeko/1.0",
+			platform = "Fuchas " .. _VERSION
+		},
+		document = {
+			getElementById = function(id)
+				for k, v in pairs(objects) do
+					if v.tag and v.tag.attr.id == id then
+						return objectWrapper(v)
+					end
+				end
+			end
+		},
+		math = math
+	}
+end
+
+local function cleanScripts()
+	for k, v in pairs(scriptProcesses) do
+		v:kill()
+	end
+end
+
+makeScriptEnv()
+
+local function loadScripts(tag)
+	for _, v in pairs(tag.childrens) do
+		if v.name == "#text" and v.parent.name == "script" and (not v.parent.attr.lang or v.parent.attr.lang == "application/lua") then
+			local chunk, err = load(v.unformattedContent, "web-script", "t", scriptEnv)
+			if not chunk then
+				error(err)
+			end
+			local process = require("shin32").newProcess("luaweb-script-" .. scriptId, chunk)
+			table.insert(scriptProcesses, process)
+			scriptId = scriptId + 1
+		else
+			loadScripts(v)
+		end
+	end
+end
+
 local function resolve(tag)
 	for _, v in pairs(tag.childrens) do
 		if v.attr.x then
@@ -46,8 +128,11 @@ local function resolve(tag)
 					x = cx,
 					y = cy,
 					text = v.content,
-					hyperlink = v.parent.attr.href
+					hyperlink = v.parent.attr.href,
+					tag = v.parent
 				})
+			elseif v.parent.name == "script" then
+				-- do nothing
 			else
 				table.insert(objects, {
 					type = "text",
@@ -61,6 +146,16 @@ local function resolve(tag)
 			cx = 1
 			cy = cy + 1
 			resolve(v)
+		elseif v.name == "canvas" then
+			table.insert(objects, {
+				type = "canvas",
+				x = cx,
+				y = cy,
+				width = v.attr.width or 16,
+				height = v.attr.height or 8,
+				drawHandler = nil,
+				tag = v
+			})
 		else
 			resolve(v)
 			if v.name == "text" or v.name == "h1" or v.name == "h2" or v.name == "h3" or v.name == "h4" or v.name == "h5" then
@@ -101,6 +196,40 @@ local function render()
 			end
 			gpu.set(obj.x, obj.y, obj.text)
 		end
+		if obj.type == "canvas" then
+			if obj.drawHandler == nil then
+				local bg, fg = 0x000000, 0xFFFFFF
+				obj.drawHandler = function(...)
+					local pack = table.pack(...)
+					local op, x, y, width, height = pack[1], pack[2] or 1, pack[3] or 1, pack[4] or 1, pack[5] or 1
+					x = x+obj.x-1
+					y = y+obj.y-1
+					if x < obj.x then x = obj.x end
+					if x > obj.x+obj.width then x = obj.x+obj.width end
+					if y < obj.y then y = obj.y end
+					if y > obj.y+obj.height then y = obj.y+obj.height end
+					if width < 1 then width = 1 end
+					--if x+width > obj.width then width = obj.width-x end
+					if height < 1 then heigth = 1 end
+					--if y+height > obj.height then height = obj.height-y end
+					if op == "text" then
+						gpu.setBackground(bg)
+						gpu.setForeground(fg)
+						gpu.set(x, y, pack[4])
+					end
+					if op == "fill" then
+						gpu.setBackground(bg)
+						gpu.fill(x, y, width, height, pack[6])
+					end
+					if op == "setbg" then
+						bg = pack[2]
+					end
+					if op == "setfg" then
+						fg = pack[2]
+					end
+				end
+			end
+		end
 	end
 end
 
@@ -117,12 +246,16 @@ local function go(link)
 	cx = 1
 	cy = 3
 	objects = {}
+	cleanScripts()
 	resolve(parsed)
 	render()
+	loadScripts(parsed)
 end
 
+cleanScripts()
 resolve(parsed)
 render()
+loadScripts(parsed)
 
 while true do
 	local id, a, b, c = event.pull()
@@ -143,4 +276,6 @@ while true do
 	end
 end
 
+gpu.setBackground(0x000000)
+gpu.setForeground(0xFFFFFF)
 gpu.fill(1, 1, width, height, " ")
